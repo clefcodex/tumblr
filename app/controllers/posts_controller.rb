94b1af6bbff1b6c8c770455bd866388beb6a08e0
ClefCodeX <clefcodex@gmail.com>
2018-07-04 19:45:49 +0100

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,9 +22,32 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit		
+	end
+
+	def update		
+	end
+
+	def destroy
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end 		
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private 
 
 		def post_params
 			params.require(:post).permit(:title, :body)
+		end
+
+		def find_post
+			@post = Post.find(params[:id])			
 		end
 end
